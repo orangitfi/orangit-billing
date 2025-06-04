5091495c9c7b6cf5bc51bfc44b5e3b3b7e1cc91b
Sami Bister (OrangIT) <sami.bister@orangit.fi>
2025-06-04 10:36:54 +0300

    Fix to the script

--- fixed_fee_invoicing/fixed_fee.sh
+++ fixed_fee_invoicing/fixed_fee.sh
@@ -20,13 +20,13 @@ previous_month=$(printf "%02d" $previous_month)
 
 # Construct the date string
 DATE_STR="$previous_year-$previous_month"
-
+echo "DATE_STR: $DATE_STR"
 # move to execution directory
 cd $HOME/src/orangit/orangit-billing/fixed_fee_invoicing
 # Construct and run the Python command
 uv run python -m fixed_fee_invoicing \
 	 --config $HOME/laskutus/$DATE_STR/customer.csv \
-	 --input $HOME/laskutus/$DATE_STR/passtrhough.csv \
+	 --input $HOME/laskutus/$DATE_STR/passthrough.csv \
 	 --output $HOME/laskutus/$DATE_STR/fixed-fee-$DATE_STR.csv \
 	 --month $previous_month\
 	 --year $previous_year\
